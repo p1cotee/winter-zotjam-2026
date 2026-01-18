aa05e84b5aef0c68a22ff576805fc47370da2e48
p1cotee <izzy.wang.nw@gmail.com>
2026-01-17 20:11:47 -0800

    crush randomly turns around

---
+++ winter zotjam 2026/Assets/Scripts/CrushManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CrushManager : MonoBehaviour
+{
+    public Sprite crushLookingAway;
+    public Sprite crushLookingAtPlayer;
+    public GameObject crush;
+    SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = crush.GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(randomTurnArounds());
+    }
+    
+    IEnumerator randomTurnArounds()
+    {
+        while (true){
+            float waitTime = Random.Range(1f, 5f);
+            yield return new WaitForSeconds(waitTime);
+
+            sr.sprite = crushLookingAtPlayer;
+
+            yield return new WaitForSeconds(3f);
+            sr.sprite = crushLookingAway;
+        }
+    }
+}

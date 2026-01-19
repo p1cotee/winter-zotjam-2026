b82d76cac965025d0a8b201e3c08446ea1431cb0
Zoey Chen <56212261+zochen@users.noreply.github.com>
2026-01-18 16:08:12 -0800

    bad ending screen buttons work i think

---
+++ winter zotjam 2026/Assets/Scripts/EndingManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingManager : MonoBehaviour
+{
+	public void OnHomeClick() {
+		SceneManager.LoadScene("TitleScreen");
+	}
+
+	public void OnClick() {
+        SceneManager.LoadScene("IntroScene");
+	}
+}
